a2bf3de90651f944429a32bda8e4b158c0ecb1eb
YounessHassoune <yhassoune7@gmail.com>
2021-07-30 22:19:39 +0100

    adding dashboard routes

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
 import NotFound from '../views/NotFound.vue'
-
+import UserDashboard from '../views/Individual/UserDashboard.vue'
+import BuisnessDashboard from '../views/Business/BuisnessDashboard.vue'
 const routes = [
   {
     path: '/',
@@ -15,10 +16,21 @@ const routes = [
     component: Register
   },
   {
+    path: '/userdashboard',
+    name: 'UserDashboard',
+    component: UserDashboard
+  },
+  {
+    path: '/buisnessdashboard',
+    name: 'BuisnessDashboard',
+    component: BuisnessDashboard
+  },
+  {
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: NotFound
   },
+
 
 ]
 
