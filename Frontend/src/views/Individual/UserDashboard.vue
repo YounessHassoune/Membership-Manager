db9aff9d7243624583ed78b348cce49a0fa702d9
YounessHassoune <yhassoune7@gmail.com>
2021-07-30 22:24:24 +0100

    adding dashboard views 😎

---
+++ Frontend/src/views/Individual/UserDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/scss/_placeholders.scss";
+@import url("https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i");
+.container {
+  @extend %container;
+}
+</style>
